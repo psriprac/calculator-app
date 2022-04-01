3a40fefa5ed4bdf49df75964d573c1f13d01f5dc
psriprac <psriprac@gmail.com>
2022-04-01 01:18:01 -0700

    finished functionality

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 const add = (x, y) => {
-    return parseInt(x) + parseInt(y);
+    return parseFloat(x) + parseFloat(y);
 }
 
 const subtract = (x, y) => {
-    return parseInt(x) - parseInt(y);
+    return parseFloat(x) - parseFloat(y);
 }
 
 const multiply = (x, y) => {
-    return parseInt(x) * parseInt(y);
+    return parseFloat(x) * parseFloat(y);
 }
 
 const divide = (x, y) => {
-    if(parseInt(y) === 0) {
-        return 'No can do partner';
+    if(parseFloat(y) === 0) {
+        alert('Yeah, no.')
+        return x;
     } else {
-        return parseInt(x) / parseInt(y);
+        return parseFloat(x) / parseFloat(y);
     }
 }
 
 const operate = (op, x, y) => {
     switch (op) {
         case '+':
-            return add(x, y);
+            return add(x, y).toPrecision(3);
         case '-':
-            return subtract(x, y);
+            return subtract(x, y).toPrecision(3);
         case 'x' || '*':
-            return multiply(x, y);
+            return multiply(x, y).toPrecision(3);
         case '/':
-            return divide(x, y);
+            return divide(x, y).toPrecision(3);
     }
 }
 
 const maxLength = 12;
+let resultsLock = false;
 
 const resultsViewport = document.querySelector('#results-viewport')
-resultsViewport.textContent = '';
+resultsViewport.textContent = '0';
 
 const feedViewport = document.querySelector('#feed-viewport')
 feedViewport.textContent = '';
@@ -46,6 +48,10 @@ one.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 1;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 1;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 1;
         }
@@ -59,7 +65,11 @@ two.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 2;
-        } else {
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 2;
+            resultsLock = false;
+        }else {
             resultsViewport.textContent += 2;
         }
     }
@@ -72,7 +82,11 @@ three.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 3;
-        } else {
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 3;
+            resultsLock = false;
+        }else {
             resultsViewport.textContent += 3;
         }
     }
@@ -85,6 +99,10 @@ four.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 4;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 4;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 4;
         }
@@ -98,6 +116,10 @@ five.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 5;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 5;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 5;
         }
@@ -111,6 +133,10 @@ six.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 6;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 6;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 6;
         }
@@ -124,6 +150,10 @@ seven.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 7;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 7;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 7;
         }
@@ -137,6 +167,10 @@ eight.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 8;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 8;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 8;
         }
@@ -150,6 +184,10 @@ nine.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 9;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 9;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 9;
         }
@@ -163,8 +201,25 @@ zero.addEventListener('click', () => {
         if(resultsViewport.textContent === '0') {
             clearResults();
             resultsViewport.textContent = 0;
+        } else if (resultsLock) {
+            clearResults();
+            resultsViewport.textContent = 0;
+            resultsLock = false;
         } else {
             resultsViewport.textContent += 0;
+        }
+    }
+});
+
+const decimal = document.querySelector('#decimal');
+
+decimal.addEventListener('click', () => { 
+    if(!resultsViewport.textContent.includes('.')) {
+        if (resultsLock) {
+            clearResults();
+            resultsLock = false;
+        } else {
+            resultsViewport.textContent += '.';
         }
     }
 });
@@ -183,6 +238,8 @@ function clearFeed() {
 }
 
 let expression = [];
+let operands = [];
+let operator = '';
 
 const addButton = document.querySelector('#add');
 const subtractButton = document.querySelector('#subtract');
@@ -191,20 +248,182 @@ const divideButton = document.querySelector('#divide');
 
 const equals = document.querySelector('#equals');
 const clear = document.querySelector('#clear');
+const del = document.querySelector('#delete');
+
+function resetExpression(){
+    expression = [];
+    operands = [];
+    operator = '';
+}
 
 clear.addEventListener('click', () => {
     clearViewports();
-    expression = [];
+    resetExpression();
 });
 
 addButton.addEventListener('click', () => {
-    let displayText = parseInt(resultsViewport.textContent);
-    feedViewport.textContent += `${displayText} + `;
-    if(feedViewport.textContent !== ''){
-        clearResults();
+    clearFeed();
+    operands.push(parseFloat(resultsViewport.textContent));
+
+    if (operator != '') {
+        operands[0] = operate(operator, operands[0], operands[1]);
+        operands.pop();
+        expression.pop();
+        clearFeed();
+    }
+
+    operator = '+';
+    
+    expression.push(operands[0]);
+    expression.push(operator);
+
+    feedViewport.textContent += `${operands[0]} + `;
+
+    resultsLock = true;
+
+    if (operands.length > 1) {
+        operands.push(parseFloat(resultsViewport.textContent));
+        let result = operate(operator, operands[0], parseFloat(resultsViewport.textContent));
+        if(!resultsLock) {
+            resultsLock = true;
+        }
+        resetExpression();
+        operands.push(result);
+        operator = '+';
+        expression.push(operands[0]);
+        expression.push(operator);
+        clearFeed();
+        feedViewport.textContent += `${operands[0]} + `;
+    }
+});
+
+subtractButton.addEventListener('click', () => {
+    operands.push(parseFloat(resultsViewport.textContent));
+
+    if (operator != '') {
+        operands[0] = operate(operator, operands[0], operands[1]);
+        operands.pop();
+        expression.pop();
+        clearFeed();
+    }
+
+    operator = '-';
+    
+    expression.push(operands[0]);
+    expression.push(operator);
+
+    feedViewport.textContent += `${operands[0]} - `;
+
+    resultsLock = true;
+
+    if (operands.length > 1) {
+        operands.push(parseFloat(resultsViewport.textContent));
+        let result = operate(operator, operands[0], parseFloat(resultsViewport.textContent));
+        if(!resultsLock) {
+            resultsLock = true;
+        }
+        resetExpression();
+        operands.push(result);
+        operator = '-';
+        expression.push(operands[0]);
+        expression.push(operator);
+        clearFeed();
+        feedViewport.textContent += `${operands[0]} - `;
+    }
+});
+
+multiplyButton.addEventListener('click', () => {
+    operands.push(parseFloat(resultsViewport.textContent));
+
+    if (operator != '') {
+        operands[0] = operate(operator, operands[0], operands[1]);
+        operands.pop();
+        expression.pop();
+        clearFeed();
+    }
+
+    operator = 'x';
+    
+    expression.push(operands[0]);
+    expression.push(operator);
+
+    feedViewport.textContent += `${operands[0]} x `;
+
+    resultsLock = true;
+
+    if (operands.length > 1) {
+        operands.push(parseFloat(resultsViewport.textContent));
+        let result = operate(operator, operands[0], parseFloat(resultsViewport.textContent));
+        if(!resultsLock) {
+            resultsLock = true;
+        }
+        resetExpression();
+        operands.push(result);
+        operator = 'x';
+        expression.push(operands[0]);
+        expression.push(operator);
+        clearFeed();
+        feedViewport.textContent += `${operands[0]} x `;
+    }
+});
+
+divideButton.addEventListener('click', () => {
+    operands.push(parseFloat(resultsViewport.textContent));
+
+    if (operator != '') {
+        operands[0] = operate(operator, operands[0], operands[1]);
+        operands.pop();
+        expression.pop();
+        clearFeed();
+    }
+
+    operator = '/';
+    
+    expression.push(operands[0]);
+    expression.push(operator);
+
+    feedViewport.textContent += `${operands[0]} / `;
+
+    resultsLock = true;
+
+    if (operands.length > 1) {
+        operands.push(parseFloat(resultsViewport.textContent));
+        let result = operate(operator, operands[0], parseFloat(resultsViewport.textContent));
+        if(!resultsLock) {
+            resultsLock = true;
+        }
+        resetExpression();
+        operands.push(result);
+        operator = '/';
+        expression.push(operands[0]);
+        expression.push(operator);
+        clearFeed();
+        feedViewport.textContent += `${operands[0]} / `;
     }
 });
 
 equals.addEventListener('click', () => {
+    operands.push(parseFloat(resultsViewport.textContent));
+    if(feedViewport.textContent === '') {
+        feedViewport.textContent = operands[0];
+        resultsLock = true;
+        resetExpression();
+        clearFeed();
+    } else if(!feedViewport.textContent.includes('=')){
+        feedViewport.textContent += `${operands[1]} = `
+        resultsViewport.textContent = operate(operator, operands[0], operands[1]);
+        resultsLock = true;
+        resetExpression();
+        if(expression === []){
+            clearFeed();
+        }
+    }   
 });
 
+del.addEventListener('click', () => {
+    if(resultsViewport.textContent.length > 1) {
+        resultsViewport.textContent = resultsViewport.textContent.slice(0, -1);
+    } else if (resultsViewport.textContent.length === 1) {
+        resultsViewport.textContent = 0;
+    }
+});
